7df5e26df285792999b59b61914188c90c08e038
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-09 01:43:43 +0200

    Цикл DO While. 1-2. Зобразіть синтаксичний шаблон циклу While. do { // виконання блоку коду } while (умова);

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main ()
 {
-    int n;
-    n = 2;
-    while (n < 15) //
-    {
-        n = n + 2;
-        cout << n << " ";
-    }
-
+    int i = 1; // Ініціалізуємо лічильник циклу
+    int sum = 1; // ініціалізуємо лічильник суми
+    do { // виконуємо цикл
+        i++;
+        sum += 1;
+    } while (i < 100); // виконується умова.
+    cout << "sum=" << sum, cout << "\niteration: " << i;
     return 0;
 }
