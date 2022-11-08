262f7b61cb5833c36528f412302b943e94f3640a
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-08 22:40:29 +0200

    1. Умова на базі циклу while: 2.Цикл виводить порядок чисел від 2 до 11. Не від 1, тому що спочатку коду number++   `++`// Інкремент. Збільшує значення змінної. Зворотну операцію називають **декремент** (зменшення)   `--` // декремент

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    number = 1;
+            while (number < 11)
+            {
+                number++;
+                cout << number << endl;
+            }
+    return 0;
+}
