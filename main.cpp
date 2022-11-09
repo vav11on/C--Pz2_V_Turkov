1185d78492d22bd571da130d6766c92cac2faa10
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-09 02:23:52 +0200

    Цикл DO While. 3. Цей код виводить 1                Так як int = 1                Якщо написати наприклад 10, у консолі буде від 1 до 10, і так з будь-яким числом крім відємних. Висновки: я вивчив цикли while, while-do. Навчився рахувати кількість ітерацій у коді, вивчив нові оператори мови С++. Я змінював структуру коду щоб знайти потрібне мені число, знаходив хибний оператор у коді, змінював на потрібний. Створив прості програми на базі циклів.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
-    int i = 1; // Ініціалізуємо лічильник циклу
-    int sum = 1; // ініціалізуємо лічильник суми
-    do { // виконуємо цикл
+    int i, n;
+    cin >> n;
+    i = 1;
+    do
+    {
+        cout << i;
         i++;
-        sum += 1;
-    } while (i < 100); // виконується умова.
-    cout << "sum=" << sum, cout << "\niteration: " << i;
+    } while (i <= n);
+
     return 0;
 }
